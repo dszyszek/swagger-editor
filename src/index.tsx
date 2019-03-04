3ae910226683aa2bb1765948cfa73bde82ba1bcd
Karol Maciaszek <karol@maciaszek.pl>
2019-03-04 01:26:18 +0100

    Solution scaffolding

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>Swagger Editor Assignment</div>,
+  document.getElementById("root")
+);
---
+++ src/util/linting.ts
@@ -0,0 +1,9 @@
+import { Spectral } from '@stoplight/spectral';
+import { oas2Functions, oas2Rules } from '@stoplight/spectral/rulesets/oas2';
+
+export function diagnose(spec: object) {
+  const spectral = new Spectral();
+  spectral.addFunctions(oas2Functions());
+  spectral.addRules(oas2Rules());
+  return spectral.run(spec);
+}
