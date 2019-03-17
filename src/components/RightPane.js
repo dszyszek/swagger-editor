37e414592b62bf44936ffcedc08d18cc0ef3b6e2
Damian <37978110+dszyszek@users.noreply.github.com>
2019-03-17 19:42:38 +0100

    Add functionality to status box (see what errors occurred)

--- src/components/RightPane.js
+++ src/components/RightPane.js
@@ -9,8 +9,7 @@ import {addToState, getCurrentState} from '../actions/jsonActions';
 import {setError, clearErrors} from '../actions/errorActions';
 import {diagnose} from '../utils/linting';
 
-
-class RightPane extends React.Component {
+    class RightPane extends React.Component {
 
         constructor() {
             super();
@@ -18,7 +17,8 @@ class RightPane extends React.Component {
             this.state = {
                 textarea: '',
                 toHighlight: '',
-                fontSize: 12
+                fontSize: 12,
+                errorBoxVisibility: true
             };
 
             this.setValueOfInput = this.setValueOfInput.bind(this);
@@ -26,6 +26,12 @@ class RightPane extends React.Component {
             this.checkIfJson = this.checkIfJson.bind(this);
             this.preventNativeTab = this.preventNativeTab.bind(this);
             this.changeFontSize = this.changeFontSize.bind(this);
+            this.changeVisibility = this.changeVisibility.bind(this);
+            this.populateStatusErrors = this.populateStatusErrors.bind(this);
+        }
+
+        componentWillMount() {
+            this.props.getCurrentState();
         }
 
         setValueOfInput(e) {
@@ -76,6 +82,37 @@ class RightPane extends React.Component {
             return true;
         }
 
+        changeVisibility(e) {           // toggle visibility of status box
+            this.setState(prev => ({
+                ...prev,
+                errorBoxVisibility: !prev.errorBoxVisibility
+            }));
+        }
+
+        populateStatusErrors(option) {      // this function will handle adding labels in status box
+            if (option === 'error') {
+                for (let o in this.props.errors.error) {
+                    return (
+                        <div className='text-center header' style={{width: '100%', height: '10%', borderBottom: '1px solid #272727', overflow: 'hidden'}}> <p style={{textAlign: 'center', lineHeight: '2.2rem', fontSize: '12px'}}> {this.props.errors.error[o]} </p> </div>
+                    );
+                }
+            } else if (option === 'lintError') {
+
+                return this.props.errors.lintError.map((e, i) => {
+                    return <div 
+                            className='text-center header' 
+                            key={i} 
+                            style={{width: '100%', height: '10%', borderBottom: '1px solid #272727', overflow: 'hidden'}} 
+                            >
+                                <p style={{textAlign: 'center', lineHeight: '2.2rem', fontSize: '12px'}}> {`Name: ${e.name}, Path: ${e.path.join('.') || '----'}`} </p> 
+                            </div>
+                });
+            } else {
+                return <div className='text-center header' style={{width: '100%', height: '10%', borderBottom: '1px solid #272727', overflow: 'hidden'}}> <p style={{textAlign: 'center', lineHeight: '2.2rem', fontSize: '12px'}}> Good job! </p> </div>;
+            }
+            
+        }
+
         validate(event) {           // this function will choose wether to lint json input or terminate everything and throw error
             const jsonValue = this.state.textarea;
 
@@ -98,7 +135,7 @@ class RightPane extends React.Component {
                 if (errorPosition) {
                     this.props.setError({jsonError: `Rendering terminated: syntax error on position: ${errorPosition} (check highlighted part)`}, 'SET_ERROR');
                 }
-
+                
                 const min = Math.max(parseInt(errorPosition) - 30, 0);
                 const max = Math.min(parseInt(errorPosition) + 30, this.state.textarea.length);
 
@@ -110,17 +147,15 @@ class RightPane extends React.Component {
             }
         }
 
-
         render() {
             return (
             <div className="col-9 px-0">
-
                     <pre
-                        className='m-0 normalizeElements normalizePre'
+                        className='m-0 normalizeElements normalizePre' // pre tag exists as 'shadow' of textarea; background color (while syntax error occurred) comes from there (the whole idea comes from a fact that <textarea> is not capable to highlight anything by default)
                         ref='preElement'
                         style={{fontSize: `${this.state.fontSize}px`}}
                     >
-
+                        
                         {reactStringReplace(this.state.textarea, this.state.toHighlight, (match, i) => {
                             return <span key={i} style={{ backgroundColor: '#dd8686', color: '#dd8686' }}>{match}</span>
                         })}
@@ -145,10 +180,33 @@ class RightPane extends React.Component {
                             ReactDOM.findDOMNode(this.refs.preElement).scrollTop = e.currentTarget.scrollTop; // synchronise scrollTop value of pre and textarea tags
                         }}
                     ></textarea>
-
+                    
                     <div className='m-0 text-center viewButtonField'> 
                         <button val='1' className='w-50 h-100 p-0 m-0 viewButton' onClick={this.changeFontSize} disabled={this.state.fontSize >= 16}>+</button>
                         <button val='-1' className='w-50 h-100 p-0 m-0 viewButton' onClick={this.changeFontSize} disabled={this.state.fontSize <= 8}>-</button>
+                    </div>
+
+                    <div className='absoluteRightBottom'>
+                        
+                        <div 
+                            className={classnames({
+                                'd-none': this.state.errorBoxVisibility
+                            })} 
+                            style={{width: '100%', height: '92%', backgroundColor: '#565656', overflow: 'scroll', border: '2px solid #272727'}}
+                        >
+                            {!_.isEmpty(this.props.errors.error) ? this.populateStatusErrors('error') : (!_.isEmpty(this.props.errors.lintError) ? this.populateStatusErrors('lintError') : this.populateStatusErrors('good'))}
+
+                        </div>
+                        
+                        <button className='m-0 pt-1 text-center statusBox' onClick={this.changeVisibility}> Status:
+                            {
+                                !_.isEmpty(this.props.errors.error) ?   // check if syntax errors exists
+                                    ` ${Object.keys(this.props.errors.error).length} error`: // if yes, return how many
+                                        (!_.isEmpty(this.props.errors.lintError) ?  // if not, check if lint errors exists
+                                            ` ${Object.keys(this.props.errors.lintError).length} errors` :
+                                                ' OK') // if everything above is false, then input text is in order
+                            }
+                        </button>
                     </div>
                     
             </div>
@@ -156,9 +214,9 @@ class RightPane extends React.Component {
         }
     }
 
-    const mapStateToProps = state => ({
-        json: state.json,
-        errors: state.errors
-    });
-    
-    export default connect(mapStateToProps, {addToState, setError, getCurrentState, clearErrors})(RightPane);
+const mapStateToProps = state => ({
+    json: state.json,
+    errors: state.errors
+});
+
+export default connect(mapStateToProps, {addToState, setError, getCurrentState, clearErrors})(RightPane);
